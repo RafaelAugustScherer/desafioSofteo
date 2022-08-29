dd6f653f29d94345eb8307e56be463a02559bd0b
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-08-29 11:52:04 -0300

    🎉 First Commit: Add base React Front-end

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { BsJournalText } from 'react-icons/bs';
+
+const Header = () => (
+  <header className="App-header">
+    <BsJournalText />
+    <p>Caderneta Online</p>
+  </header>
+);
+
+export default Header;
