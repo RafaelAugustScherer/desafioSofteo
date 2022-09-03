6db8fed214651a281a5a587b6e06f6db0a5b2a5a
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-09-02 23:53:36 -0300

    feat(Back): :package: Added cors to Express Server

--- back-end/src/app/app.ts
+++ back-end/src/app/app.ts
@@ -1,10 +1,12 @@
 import Express from 'express';
+import Cors from 'cors';
 import 'express-async-errors';
 import connectToDatabase from './model/connection';
 import appRouter from './router';
 
 const App = Express();
 App.use(Express.json());
+App.use(Cors());
 App.use(appRouter);
 
 connectToDatabase();
